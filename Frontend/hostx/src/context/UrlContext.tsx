cbc6f787d9a572aa9e4a2cebc9b582ddb37c8353
frankie-powers <franklin.power21@gmaul.com>
2024-10-16 02:07:30 +0100

    added use context

---
+++ Frontend/hostx/src/context/UrlContext.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import * as React from "react";
+
+export const UrlContext = React.createContext(null);
+
+export function useUrl() {
+  return React.useContext(UrlContext);
+}
+
+export function UrlProvider({ children }) {
+  const [url, setUrl] = React.useState("null");
+
+  return (
+    <UrlContext.Provider value={{ url, setUrl }}>
+      {children}
+    </UrlContext.Provider>
+  );
+}
